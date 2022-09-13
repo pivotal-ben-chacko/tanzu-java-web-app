4241f05e7b7d29ac9db3cae3d1ac48e65c79671d
Ben Chacko <bchacko@pivotal.io>
2022-09-13 01:20:12 +0000

    added washington

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + DC Hackathon 2022!";
+		return "Greetings from Spring Boot + Tanzu + Washington DC Hackathon 2022!";
 	}
 
 }
