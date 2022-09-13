6f83665df499c293933659f20093d2236f864dce
Ben Chacko <bchacko@pivotal.io>
2022-09-13 00:29:30 +0000

    add year

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu + DC Hackathon!";
+		return "Greetings from Spring Boot + Tanzu + DC Hackathon 2022!";
 	}
 
 }
